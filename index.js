2eeac00d83a2b60039cb2129858f1f47d3b42a7f
kenedy4 <kennedymusyoki8@gmail.com>
2024-06-30 18:45:01 +0300

    Scope lab complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+// index.js
+
+//
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+var bestCustomer;
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(newCustomer) {
+  bestCustomer = newCustomer;
+}
+
+const leastFavoriteCustomer = "initial customer";
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "new customer"; // This will cause an error
+}
